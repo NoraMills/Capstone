a7d20cf5a9c7fb5706360ffb4252e5536dc412b6
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-10 23:54:49 -0500

    tested api_views with boilerplate api code, working with hardcoded products file

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+
 
 # function based views
 
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>',
+    ]
+    return Response(routes)
+
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
