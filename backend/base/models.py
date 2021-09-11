3a84c0087a79d47a0df8efff1f305128c46fd7b3
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-11 02:02:46 -0500

    created product class

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    # image = need to set up file upload system
+    _id = models.AutoField(primary_key=True, editable=False)
+    description = models.TextField(null=True, blank=True)
