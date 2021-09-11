f23f08a2170cae1293795715a802056272529692
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-11 00:15:00 -0500

    db set up, initial migration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'commission_db',
+        'USER': 'root',
+        'PASSWORD': 'ThisIsAnUnsecurePassWord',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
