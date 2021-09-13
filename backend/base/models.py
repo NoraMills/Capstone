cfea58c9493642a44fec0684fa011169e7d53636
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-12 21:24:58 -0500

    migrated data, set up image field

--- backend/base/models.py
+++ backend/base/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
-    # image = need to set up file upload system
+    image = models.ImageField(null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
     description = models.TextField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
