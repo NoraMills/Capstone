f47e34019ded43170ffa6a7e3434f7f560080c28
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-13 09:18:55 -0500

    corrected return for ShippingAddress class

--- backend/base/models.py
+++ backend/base/models.py
@@ -67,7 +67,7 @@ class ShippingAddress(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.adress)
+        return str(self.address)
 
 
 class PaintQuality(models.Model):
