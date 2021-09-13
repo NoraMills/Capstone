5912e73f3dadb6a20dc3e524d437e1c899b33d16
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-13 16:56:37 -0500

    created navbar, cart/login links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer';
 
@@ -6,8 +8,10 @@ function App() {
   return (
     <div>
       <Header />
-      <main>
-      <h1>My app thing</h1>
+      <main className='py-3'>
+        <Container>
+          <h1>My app thing</h1>
+        </Container>
       </main>
       <Footer />
     </div>
