676f2d8e3de5dabbc12cfab9de3944e939ccc5f9
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-09-15 21:39:37 -0500

    create user working; error handling for existing user/email set up

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,6 +10,8 @@ from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -30,23 +32,24 @@ class MyTokenObtainPairView(TokenObtainPairView):
 # function based views
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
 
-        '/api/products/upload/',
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
 
-        '/api/products/<id>/reviews/',
+        )
 
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>',
-    ]
-    return Response(routes)
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
